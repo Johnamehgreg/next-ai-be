b8f7f08a55f5c11887b0b118b0c880e435459f07
John Greg <johnamehgreg@gmail.com>
2024-10-19 15:19:51 +0100

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ProductsModule } from './products/products.module';
     MongooseModule.forRoot('mongodb://localhost:27017/Ai'),
     PostsModule,
     ProductsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
